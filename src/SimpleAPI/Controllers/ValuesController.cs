06fabb3b82487b730870af62dc8461dd80e17db3
Anders Strömberg <coltlaboy@yahoo.se>
2020-09-07 11:46:52 +0200

    Unit test i now working again =)

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
 		[HttpGet("{id}")]
 		public ActionResult<string> Get(int id)
 		{
-			return "Andy Daniel Backman";
+			return "Andy Backman";
 		}
 
 		// POST api/values
